62a51066364777e938ec7868ba0c24cb8a2f4a2c
Maksim Vorontcov <maksim.vorontcov@NetCracker.com>
2017-09-15 16:49:07 +0300

    Replaced separate distances with L_k-norm

--- distance_tests.py
+++ distance_tests.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial.distance import euclidean, cityblock
+from sklearn import metrics
 
 
 def generateClusters(number, features):
@@ -14,13 +15,28 @@ def generateClusters(number, features):
     return np.concatenate((cluster1, cluster2, cluster3, cluster4, cluster5, cluster6))
 
 
-def kmeans(objs, clusters_number, centers=None, distance_fun=cityblock):
+def lk_norm(x, y, k):
+    return (sum([np.math.fabs(x_i - y_i) ** k for x_i, y_i in zip(x, y)])) ** (1/k)
+
+
+def kmeans(objs, clusters_number, k, centers=None):
+    def split_clusters(objs, clusters_number, centers, k):
+        clusters = [[] for i in range(clusters_number)]
+        obj_distrib = [-1] * len(objs)
+        for i, obj in enumerate(objs):
+            distances = [lk_norm(center, obj, k) for center in centers]
+            clust_number = np.argmin(distances)
+            clusters[clust_number].append(obj)
+            obj_distrib[i] = clust_number
+        return clusters, obj_distrib
+
     dimension = len(objs[0])
+    obj_distrib = None
     if centers is None:
         # avoid the situation when any cluster is empty
         while True:
             centers = np.random.uniform(0, 15, clusters_number * dimension).reshape((-1, dimension))
-            clusters = split_clusters(objs, clusters_number, centers, distance_fun=distance_fun)
+            clusters, obj_distrib = split_clusters(objs, clusters_number, centers, k)
             if all([len(cluster) > 0 for cluster in clusters]):
                 break
 
@@ -28,38 +44,32 @@ def kmeans(objs, clusters_number, centers=None, distance_fun=cityblock):
         # cluster and centers are already initialized
         new_centers = [np.mean(cluster, axis=0) for cluster in clusters]
         if np.equal(centers, new_centers).all():
-            return centers, clusters
+            return centers, clusters, obj_distrib
         centers = new_centers
-        clusters = split_clusters(objs, clusters_number, centers, distance_fun=distance_fun)
+        clusters, obj_distrib = split_clusters(objs, clusters_number, centers, k)
 
 
-def split_clusters(objs, clusters_number, centers, distance_fun=cityblock):
-    clusters = [[] for i in range(clusters_number)]
-    for obj in objs:
-        distances = [distance_fun(center, obj) for center in centers]
-        clusters[np.argmin(distances)].append(obj)
-    return clusters
+def do_test(elems_number, features, k):
+    def compute_error(clusters, centres):
+        error = 0
+        for index, result_center in enumerate(centres):
+            # print(result_center)
+            for obj in clusters[index]:
+                error += euclidean(obj, result_center)
 
+        return error
 
-def compute_error(clusters, centres):
-    error = 0
-    for index, result_center in enumerate(centres):
-        # print(result_center)
-        for obj in clusters[index]:
-            error += euclidean(obj, result_center)
-
-    return error
-
-
-def do_test(elems_number, features, distance_func):
     objs = generateClusters(elems_number, features)
-    result_centers, result_clusters = kmeans(objs, 6, None, euclidean)
+    result_centers, result_clusters, obj_distrib = kmeans(objs, 6, k)
     error = compute_error(result_clusters, result_centers)
-    print("Distance: " + distance_func.__name__ + ". Features: " + str(features) + ". Error: " + str(error))
+    metric = metrics.calinski_harabaz_score(objs, obj_distrib)
+    print("k-value: " + str(k) + ". Features: " + str(features) + ". Error: " + str(error)
+          + ". Calinski&Harabaz score: " + str(metric))
 
 
 if __name__ == '__main__':
     # for i in range(15, 20):
-    do_test(5000, 20, cityblock)
-    do_test(5000, 20, euclidean)
+    do_test(700, 20, 0.3)
+    do_test(700, 20, 2)
     print()
+
